0f033ea0626553360b225f91cd706d8dcbae431e
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-02 13:04:23 +0545

    update this file

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,7 +1,6 @@
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { SelectedPage } from "@/shared/types";
 import ActionButton from "@/shared/ActionButton";
-import HomePageText from "@/assets/HomePageText.png";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
@@ -39,7 +38,9 @@ const Home = ({ setSelectedPage }: Props) => {
           >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
-                <img alt="home-page-text" src={HomePageText} />
+                <h1 className="text-6xl font-bold">
+                  THE <span className="text-primary-500">FITNESS</span> CLUB
+                </h1>
               </div>
             </div>
 
