3d33b53f0e4346e80fd1b2a67c1b4d3aa163719d
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-02 13:04:15 +0545

    update this file

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -5,24 +5,28 @@ const Footer = () => {
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
-          <img alt="logo" src={Logo} />
+          {/* <img alt="logo" src={Logo} className="w-36" /> */}
+          <h1 className="text-6xl font-bold">
+            <span className="text-primary-500">FITNESS</span>
+          </h1>
           <p className="my-5">
-            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
-            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
-            orci ut habitant laoreet. Iaculis tristique.
+            At FITNESS, it is more than just working out—it's about building a
+            better quality of life. We are dedicated to providing the perfect
+            environment, the best tools, and expert guidance to help you unlock
+            your full potential and achieve holistic wellness.
           </p>
-          <p>© Evogym All Rights Reserved.</p>
+          <p>© Fitness All Rights Reserved.</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
-          <p className="my-5">Massa orci senectus</p>
-          <p className="my-5">Et gravida id et etiam</p>
-          <p>Ullamcorper vivamus</p>
+          <p className="my-5">Home</p>
+          <p className="my-5">Benifits</p>
+          <p>Classes</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
-          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-          <p>(333)425-6825</p>
+          <p className="my-5">fitness@gmail.com</p>
+          <p>+977-987654321</p>
         </div>
       </div>
     </footer>
