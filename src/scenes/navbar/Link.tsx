d0c4f7fc222c77803bd613516bd58625fc4006bd
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-02 13:04:30 +0545

    update this file

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -12,8 +12,10 @@ const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
 
   return (
     <AnchorLink
-      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
-        transition duration-500 hover:text-primary-300
+      className={`${
+        selectedPage === lowerCasePage ? " text-primary-500" : "font-medium"
+      }
+        transition duration-300 hover:text-primary-300
       `}
       href={`#${lowerCasePage}`}
       onClick={() => setSelectedPage(lowerCasePage)}
